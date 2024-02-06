92b08e45c964081aa12a24f1b1f24e2e72cd98f1
jacob-alfaro49 <jacobalfaro969@gmail.com>
2024-02-05 20:44:30 -0800

    This is our second commit with comments added to SudokuSquareDemo.java

--- src/SudokuSquareDemo.java
+++ src/SudokuSquareDemo.java
@@ -1,6 +1,6 @@
-public class SudokuSquareDemo {
+public class SudokuSquareDemo { //Jacob Alfaro
     public static void main (String[] args){
-        int[][] sudokuSquare = {{2,7,6}, //This is the sudoku square we are testing
+        int[][] sudokuSquare = {{2,7,6}, //Any 2d array can be used to check and see if it is a magic sudoku square
                 {9,5,1},
                 {4,3,8}};
         SudokuSquare.check(sudokuSquare);
